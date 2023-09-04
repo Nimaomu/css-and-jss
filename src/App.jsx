a028b346c528d331537dabb47582a0f0ffa9dcdf
Nimaomu <nimaomuhyolmo@gmail.com>
2023-09-03 21:38:41 -0400

    task five

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
- import React from 'react'
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={ styles.div}>
+      <header className={styles.header}>
+        </header>
+        <h1>Welcome to My task five</h1>
+        <h2> CSS styling in a React Project with Vite</h2>
+        <main className={ styles.main}>
+          <div className={styles.box}>
+            </div>
+            </main>
+            <ul> 
+          <li> Review CSS styling and JavaScript Variables</li>
+			    <li> Set Up a React Project with Vite</li>
+			    <li> Apply CSS Styling</li>
+            </ul>
+            
+            <footer className={styles.footer}>
+              <p> This is where you can learn more about me and my work.</p>
+              </footer>
+
+    </div>
+  )
+}
+
+export default App 
+
+
+      
+   
  
- function App() {
-   return (
-     <div>App</div>
-   )
- }
- 
- export default App
